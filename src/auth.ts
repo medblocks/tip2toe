eb4e7292954e739e17b5f8ae2ef6cd646a952791
Sidharth Ramesh <tornadoalert@gmail.com>
2023-08-12 13:56:28 +0530

    added auth

---
+++ src/auth.ts
@@ -0,0 +1,25 @@
+import {smartAuth} from '@medblocks/appauth/src/medblocks_auth'
+
+/**
+ * To DO:
+ *  - Add default button for login with dev.medblocks.com
+ *  - Check for launch and iss, if present, automatically start auth flow
+ *  - Implement redirect to original url after auth logic with Preact routing
+ *  - Get service URLs from services
+ *  - If patient: Only show one form
+ *  - If practitioenr Show both forms
+ * 
+ */
+
+export const {accessToken, idToken, context, services } = await smartAuth({
+  iss: "https://dev.medblocks.com/fhir",
+  clientId: "tip2toe",
+  redirectUri: "http://localhost:5173",
+  scope: "emulate_user openid offline launch/patient extra",
+  onRedirect(originalUrl) {
+      console.log(originalUrl)
+  },
+})
+
+
+
