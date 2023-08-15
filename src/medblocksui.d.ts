e2af11030810ff9f4053e3c03d4b32e0ea6a7c75
anas790 <anas@medblocks.org>
2023-08-15 11:34:39 +0530

    #adding medblocks .d.ts file

--- src/medblocksui.d.ts
+++ src/medblocksui.d.ts
@@ -8,11 +8,14 @@ declare global {
     interface IntrinsicElements {
       // ["component-name"]: {props, Events}
       ['mb-input']: {path: string, data?: string, terminology: string, textarea?: Boolean, label?: string, id?: string, required?: boolean, type?: string, placeholder?: string, min?: number, max?: number, minlength?: number, maxlength?: number, disabled?: boolean, rows?: string, resize?: string, onMbInput?: (event: CustomEvent<string>) => void},
-      ['mb-form']: any,
+      ['mb-form']: {path:string, ctx?: any, novalidate?: boolean, templateId?: string, nosuggest?: boolean, variant?: string, onMbSubmit?: (event: CustomEvent<object>) => void},
       ['mb-context']: {path: string, bind?: object, label?: string},
       ['mb-buttons']: {path: string, label?: string, type?: string, data?: object, terminology?: string, disabled?: boolean, children?: any, onMbClick?: (event: CustomEvent<object>) => void},
       ['mb-option']: {value?: string, label?: string, selected?: boolean, disabled?: boolean},
       ['mb-search'] : {label?: string, disablefallback?: Boolean, plugin?: (event: CustomEvent<string>) => void},
+      ['mb-select']: {path: string, data?: string, terminology?: string, label?: string, id?: string, required?: boolean, placeholder?: string, disabled?: boolean, onMbInput?: (event: CustomEvent<string>) => void},
+      ['mb-submit']: {label?: string, disabled?: boolean, onMbClick?: (event: CustomEvent<object>) => void},
+      ['mb-textarea']: {path: string, data?: string, terminology?: string, label?: string, id?: string, required?: boolean, placeholder?: string, disabled?: boolean, rows?: string, resize?: string, onMbInput?: (event: CustomEvent<string>) => void},
     }
   }
 }
